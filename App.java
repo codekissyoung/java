9b2c0879ebf5832925c595c672a56febf52c5d8e
link <link@muchenglin.com>
2021-11-17 13:13:48 +0800

    update db config

--- App.java
+++ App.java
@@ -3,21 +3,27 @@ import inheri.Manager;
 
 public class App {
     public static void main(String[] args) {
+        var ems = new Employee[3];
+
         // 父类
         var link = new Employee("link");
         link.setAge(26);
         link.setDesc("程序员");
         link.empSalary(1000);
-        link.printEmployee();
+        ems[0] = link;
 
         // 子类
         var boss = new Manager("luke", 80000, 2021, 12, 12);
         boss.setDesc("CTO");
         boss.setBonus(1000);
-        boss.printEmployee();
+        ems[1] = boss;
 
         // 所有类的父对象
-        var obj = new Employee("max", 800);
-        obj.printEmployee();
+        var max = new Employee("max", 800);
+        ems[2] = max;
+
+        for (Employee e: ems) {
+            e.printEmployee();
+        }
     }
 }
