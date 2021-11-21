5ed9eca22e8fbce8c3b33cbfab80e25e986e1dc0
codekissyoung <1162097842@qq.com>
2021-11-21 17:19:38 +0800

    update java

--- App/App.java
+++ App/App.java
@@ -6,26 +6,26 @@ import javax.xml.XMLConstants;
 
 public class App {
     public static void main(String[] args) {
+
+        System.out.println("The square root " + "of \u03C0(π)\u2122 is " + Math.sqrt(Math.PI));
+
         var ems = new Employee[3];
 
-        // 父类
         var link = new Employee("link");
         link.setAge(26);
         link.setDesc("程序员");
         link.empSalary(1000);
         ems[0] = link;
 
-        // 子类
         var boss = new Manager("luke", 80000, 2021, 12, 12);
         boss.setDesc("CTO");
         boss.setBonus(1000);
         ems[1] = boss;
 
-        // 所有类的父对象
         var max = new Employee("max", 800);
         ems[2] = max;
 
-        for (Employee e: ems) {
+        for (var e: ems) {
             e.printEmployee();
         }
 
--- inheri/Person.java
+++ inheri/Person.java
@@ -4,14 +4,11 @@ public abstract class Person
 {
     protected String name;
     protected int age;
-
     public Person(String name)
     {
         this.name = name;
     }
-
     public abstract void run();
     public abstract String getName();
     public abstract String getDesc();
-
 }
