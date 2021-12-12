45eaba763af0b9dbb9df7094086cef588b90f62a
codekissyoung <1162097842@qq.com>
2021-12-13 02:15:24 +0800

    update

--- Lexer/Lexer.java
+++ Lexer/Lexer.java
@@ -8,36 +8,39 @@ import java.util.List;
 public class Lexer {
 
     public static void main(String[] args) {
-        Lexer lexer = new Lexer();
+        try {
+            Lexer lexer = new Lexer();
+            var script = "int age = 45;";
+            System.out.println("parse :" + script);
+            var tokenReader = lexer.tokenize(script);
+            dump(tokenReader);
 
-        var script = "int age = 45;";
-        System.out.println("parse :" + script);
-        var tokenReader = lexer.tokenize(script);
-        dump(tokenReader);
+            //测试inta的解析
+            script = "inta age = 45;";
+            System.out.println("\nparse :" + script);
+            tokenReader = lexer.tokenize(script);
+            dump(tokenReader);
 
-        //测试inta的解析
-        script = "inta age = 45;";
-        System.out.println("\nparse :" + script);
-        tokenReader = lexer.tokenize(script);
-        dump(tokenReader);
+            //测试in的解析
+            script = "in age = 45;";
+            System.out.println("\nparse :" + script);
+            tokenReader = lexer.tokenize(script);
+            dump(tokenReader);
 
-        //测试in的解析
-        script = "in age = 45;";
-        System.out.println("\nparse :" + script);
-        tokenReader = lexer.tokenize(script);
-        dump(tokenReader);
+            //测试>=的解析
+            script = "age >= 45;";
+            System.out.println("\nparse :" + script);
+            tokenReader = lexer.tokenize(script);
+            dump(tokenReader);
 
-        //测试>=的解析
-        script = "age >= 45;";
-        System.out.println("\nparse :" + script);
-        tokenReader = lexer.tokenize(script);
-        dump(tokenReader);
-
-        //测试>的解析
-        script = "age > 45;";
-        System.out.println("\nparse :" + script);
-        tokenReader = lexer.tokenize(script);
-        dump(tokenReader);
+            //测试>的解析
+            script = "age > 45;";
+            System.out.println("\nparse :" + script);
+            tokenReader = lexer.tokenize(script);
+            dump(tokenReader);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
     // 有限状态机的各种状态
@@ -69,7 +72,17 @@ public class Lexer {
         IntLiteral
     }
 
-    //下面几个变量是在解析过程中用到的临时变量,如果要优化的话，可以塞到方法里隐藏起来
+    // Token的一个简单实现只有类型和文本值两个属性
+    private static final class SimpleToken implements Token {
+        private TokenType type = null;         //Token类型
+        private String text = null;         //文本值
+        public TokenType getType() {return this.type;}
+        public String getText() {
+            return text;
+        }
+    }
+
+    // 下面几个变量是在解析过程中用到的临时变量,如果要优化的话，可以塞到方法里隐藏起来
     private StringBuffer tokenText = null;   // 临时保存token的文本
     private List<Token> tokens = null;       // 保存解析出来的Token
     private SimpleToken token = null;        // 当前正在解析的Token
@@ -84,12 +97,111 @@ public class Lexer {
         return ch == ' ' || ch == '\t' || ch == '\n';
     }
 
+    // 解析字符串，形成Token, 这是一个有限状态自动机，在不同的状态中迁移
+    public SimpleTokenReader tokenize(String codeStr) throws IOException {
+        var reader = new CharArrayReader(codeStr.toCharArray());
+        tokens = new ArrayList<Token>();
+        tokenText = new StringBuffer();
+        token = new SimpleToken();
+        int ich;
+        char ch = 0;
+
+        var state = DfaState.Initial; // 先待在初始化状态
+        while ((ich = reader.read()) != -1) {
+            // 读入了一个字符，接下来要开始生产 Token 了
+            ch = (char)ich;
+            switch (state) {
+                // 如果是初始状态，则根据读入的字符发生跃迁
+                case Initial:
+                    state = initToken(ch);
+                    break;
+                // 如果已经是标识符状态
+                case Identifier:
+                    // 如果是字母或数字，则继续保持标识符状态
+                    if (isAlpha(ch) || isDigit(ch)) {
+                        tokenText.append(ch);
+                    // 退出标识符状态，并保存Token
+                    } else {
+                        state = initToken(ch);
+                    }
+                    break;
+                case GT: // > 状态
+                    if (ch == '=') {
+                        token.type = TokenType.GE;  //转换成GE
+                        state = DfaState.GE;
+                        tokenText.append(ch);
+                    } else {
+                        state = initToken(ch);      //退出GT状态，并保存Token
+                    }
+                    break;
+                case IntLiteral:
+                    // 继续保持在数字字面量状态
+                    if (isDigit(ch)) {
+                        tokenText.append(ch);
+                    } else {
+                        state = initToken(ch);
+                    }
+                    break;
+                case GE:
+                case Assignment:
+                case Plus:
+                case Minus:
+                case Star:
+                case Slash:
+                case SemiColon:
+                case LeftParen:
+                case RightParen:
+                    state = initToken(ch);
+                    break;
+                case Id_int1:
+                    if (ch == 'n') {
+                        state = DfaState.Id_int2;
+                        tokenText.append(ch);
+                    }
+                    else if (isDigit(ch) || isAlpha(ch)){
+                        state = DfaState.Identifier;
+                        tokenText.append(ch);
+                    }
+                    else {
+                        state = initToken(ch);
+                    }
+                    break;
+                case Id_int2:
+                    if (ch == 't') {
+                        state = DfaState.Id_int3;
+                        tokenText.append(ch);
+                    }
+                    else if (isDigit(ch) || isAlpha(ch)){
+                        state = DfaState.Identifier;
+                        tokenText.append(ch);
+                    }
+                    else {
+                        state = initToken(ch);
+                    }
+                    break;
+                case Id_int3:
+                    if (isBlank(ch)) {
+                        token.type = TokenType.Int;
+                        state = initToken(ch);
+                    }
+                    else{
+                        state = DfaState.Identifier;    //切换回Id状态
+                        tokenText.append(ch);
+                    }
+                    break;
+            }
+        }
+        // 把最后一个token送进去
+        if (tokenText.length() > 0) {
+            initToken(ch);
+        }
+        return new SimpleTokenReader(tokens);
+    }
+
     /**
      * 有限状态机进入初始状态
      * 这个初始状态其实并不做停留，它马上进入其他状态
      * 开始解析的时候，进入初始状态；某个Token解析完毕，也进入初始状态，在这里把Token记下来，然后建立一个新的Token
-     * @param ch
-     * @return
      */
     private DfaState initToken(char ch) {
         if (tokenText.length() > 0) {
@@ -152,121 +264,6 @@ public class Lexer {
             newState = DfaState.Initial; // skip all unknown patterns
         }
         return newState;
-    }
-
-    /**
-     * 解析字符串，形成Token
-     * 这是一个有限状态自动机，在不同的状态中迁移
-     */
-    public SimpleTokenReader tokenize(String codeStr) {
-        tokens = new ArrayList<Token>();
-        var reader = new CharArrayReader(codeStr.toCharArray());
-        tokenText = new StringBuffer();
-        token = new SimpleToken();
-        int ich;
-        char ch = 0;
-        var state = DfaState.Initial;
-        try {
-            while ((ich = reader.read()) != -1) {
-                ch = (char)ich;
-                switch (state) {
-                    case Initial:
-                        state = initToken(ch);          //重新确定后续状态
-                        break;
-                    case Identifier:
-                        if (isAlpha(ch) || isDigit(ch)) {
-                            tokenText.append(ch);       //保持标识符状态
-                        } else {
-                            state = initToken(ch);      //退出标识符状态，并保存Token
-                        }
-                        break;
-                    case GT:
-                        if (ch == '=') {
-                            token.type = TokenType.GE;  //转换成GE
-                            state = DfaState.GE;
-                            tokenText.append(ch);
-                        } else {
-                            state = initToken(ch);      //退出GT状态，并保存Token
-                        }
-                        break;
-                    case GE:
-                    case Assignment:
-                    case Plus:
-                    case Minus:
-                    case Star:
-                    case Slash:
-                    case SemiColon:
-                    case LeftParen:
-                    case RightParen:
-                        state = initToken(ch);          //退出当前状态，并保存Token
-                        break;
-                    case IntLiteral:
-                        if (isDigit(ch)) {
-                            tokenText.append(ch);       //继续保持在数字字面量状态
-                        } else {
-                            state = initToken(ch);      //退出当前状态，并保存Token
-                        }
-                        break;
-                    case Id_int1:
-                        if (ch == 'n') {
-                            state = DfaState.Id_int2;
-                            tokenText.append(ch);
-                        }
-                        else if (isDigit(ch) || isAlpha(ch)){
-                            state = DfaState.Identifier;    //切换回Id状态
-                            tokenText.append(ch);
-                        }
-                        else {
-                            state = initToken(ch);
-                        }
-                        break;
-                    case Id_int2:
-                        if (ch == 't') {
-                            state = DfaState.Id_int3;
-                            tokenText.append(ch);
-                        }
-                        else if (isDigit(ch) || isAlpha(ch)){
-                            state = DfaState.Identifier;    //切换回id状态
-                            tokenText.append(ch);
-                        }
-                        else {
-                            state = initToken(ch);
-                        }
-                        break;
-                    case Id_int3:
-                        if (isBlank(ch)) {
-                            token.type = TokenType.Int;
-                            state = initToken(ch);
-                        }
-                        else{
-                            state = DfaState.Identifier;    //切换回Id状态
-                            tokenText.append(ch);
-                        }
-                        break;
-                    default:
-
-                }
-            }
-            // 把最后一个token送进去
-            if (tokenText.length() > 0) {
-                initToken(ch);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return new SimpleTokenReader(tokens);
-    }
-
-    /**
-     * Token的一个简单实现只有类型和文本值两个属性
-     */
-    private static final class SimpleToken implements Token {
-        private TokenType type = null;         //Token类型
-        private String text = null;         //文本值
-        public TokenType getType() {return this.type;}
-        public String getText() {
-            return text;
-        }
     }
 
     // 打印所有的Token
--- Lexer/TokenReader.java
+++ Lexer/TokenReader.java
@@ -1,20 +1,11 @@
 package Lexer;
 
-/**
- * 一个Token流。由Lexer生成。Parser可以从中获取Token。
- */
+// 一个Token流。由Lexer生成。Parser可以从中获取Token。
 public interface TokenReader{
     // 返回Token流中下一个Token，并从流中取出。 如果流已经为空，返回null;
     public Token read();
-
     public Token peek();
-
-    // Token流回退一步。恢复原来的Token。
-    public void unread();
-
-    // 获取Token流当前的读取位置。
-    public int getPosition();
-
-    // 设置Token流当前的读取位置
-    public void setPosition(int position);
+    public void unread();                  // Token流回退一步。恢复原来的Token。
+    public int getPosition();              // 获取Token流当前的读取位置。
+    public void setPosition(int position); // 设置Token流当前的读取位置
 }
