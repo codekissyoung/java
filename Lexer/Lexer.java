f25158b6f0b1a4bd2c5c9406a08fc18d7be444f9
codekissyoung <1162097842@qq.com>
2021-12-26 17:11:40 +0800

    update

--- Lexer/Lexer.java
+++ Lexer/Lexer.java
@@ -10,34 +10,36 @@ public class Lexer {
     public static void main(String[] args) {
         try {
             var lexer = new Lexer();
-            var script = "int age = 45;";
-            System.out.println("parse :" + script);
+            var script = " int age = 45;\nvar \tname = 'link';" +
+                    " if(age >= 76) {println('>');}" +
+                    "\nint level += 2 + 3 * 5;// test 注释\n";
+            System.out.println("parse :\n" + script);
             var tokenReader = lexer.tokenize(script);
             dump(tokenReader);
 
-            //测试inta的解析
-            script = "inta age = 45;";
-            System.out.println("\nparse :" + script);
-            tokenReader = lexer.tokenize(script);
-            dump(tokenReader);
-
-            //测试in的解析
-            script = "in age = 45;";
-            System.out.println("\nparse :" + script);
-            tokenReader = lexer.tokenize(script);
-            dump(tokenReader);
-
-            //测试>=的解析
-            script = "age >= 45;";
-            System.out.println("\nparse :" + script);
-            tokenReader = lexer.tokenize(script);
-            dump(tokenReader);
-
-            //测试>的解析
-            script = "age > 45;";
-            System.out.println("\nparse :" + script);
-            tokenReader = lexer.tokenize(script);
-            dump(tokenReader);
+//            //测试inta的解析
+//            script = "inta age = 45;";
+//            System.out.println("\nparse :" + script);
+//            tokenReader = lexer.tokenize(script);
+//            dump(tokenReader);
+//
+//            //测试in的解析
+//            script = "in age = 45;";
+//            System.out.println("\nparse :" + script);
+//            tokenReader = lexer.tokenize(script);
+//            dump(tokenReader);
+//
+//            //测试>=的解析
+//            script = "age >= 45;";
+//            System.out.println("\nparse :" + script);
+//            tokenReader = lexer.tokenize(script);
+//            dump(tokenReader);
+//
+//            //测试>的解析
+//            script = "age > 45;";
+//            System.out.println("\nparse :" + script);
+//            tokenReader = lexer.tokenize(script);
+//            dump(tokenReader);
         }catch (IOException e){
             e.printStackTrace();
         }
@@ -45,9 +47,9 @@ public class Lexer {
 
     // 有限状态机的各种状态
     enum fsm_state {
-        Initial,    // 初始状态
+        Init,       // 初始状态
         Identifier, // 标识符
-        ASSIG,      // =
+        Assign,      // =
         GT,         // >
         GE,         // >=
         Plus,       // +
@@ -58,9 +60,14 @@ public class Lexer {
         Id_int2,
         Id_int3,
         SemiColon,
-        LeftParen,
-        RightParen,
-        IntLiteral
+        LeftParen, // (
+        RightParen, // )
+        singleQuotes, // '
+        doubleQuotes, // "
+        leftCurlyBrace, // {
+        rightCurlyBrace, // {
+        IntLiteral,    // Int 字面量
+        StringLiteral, // String 字面量
     }
 
     // Token的一个简单实现只有类型和文本值两个属性
@@ -108,57 +115,28 @@ public class Lexer {
     }
 
     // 下面几个变量是在解析过程中用到的临时变量,如果要优化的话，可以塞到方法里隐藏起来
-    private StringBuffer tokenText = null;   // 临时保存token的文本
-    private List<Token> tokens = null;       // 保存解析出来的Token
-    private SimpleToken token = null;        // 当前正在解析的Token
+    private StringBuffer tokenTmpStr = null;   // 临时保存token的文本
+    private SimpleToken oneToken  = null;       // 当前正在解析的Token
+    private List<Token> tokenList = null;      // 保存解析出来的Token
 
     // 解析字符串，形成Token, 这是一个有限状态自动机，在不同的状态中迁移
     public SimpleTokenReader tokenize(String codeStr) throws IOException {
         var reader = new CharArrayReader(codeStr.toCharArray());
-        tokens = new ArrayList<>();
-        tokenText = new StringBuffer();
-        token = new SimpleToken();
+        tokenList = new ArrayList<>();
+        tokenTmpStr = new StringBuffer();
+        oneToken = new SimpleToken();
         int ich = 0;
         char ch = 0;
 
-        var state = Lexer.Lexer.fsm_state.Initial; // 先待在初始化状态
-        while ((ich = reader.read()) != -1) {
-            // 读入了一个字符，接下来要开始生产 Token 了
-            ch = (char)ich;
-            switch (state) {
-                // 如果是初始状态，则根据读入的字符发生跃迁
-                case Initial:
-                    state = initToken(ch);
-                    break;
-                // 如果已经是标识符状态
-                case Identifier:
-                    // 如果是字母或数字，则继续保持标识符状态
-                    if (isAlpha(ch) || isDigit(ch)) {
-                        tokenText.append(ch);
-                    // 退出标识符状态，并保存Token
-                    } else {
-                        state = initToken(ch);
-                    }
-                    break;
-                case GT: // > 状态
-                    if (ch == '=') {
-                        token.type = TokenType.GE;  //转换成GE
-                        state = Lexer.Lexer.fsm_state.GE;
-                        tokenText.append(ch);
-                    } else {
-                        state = initToken(ch);      //退出GT状态，并保存Token
-                    }
-                    break;
-                case IntLiteral:
-                    // 继续保持在数字字面量状态
-                    if (isDigit(ch)) {
-                        tokenText.append(ch);
-                    } else {
-                        state = initToken(ch);
-                    }
-                    break;
+        var state = fsm_state.Init; // 先待在初始化状态
+        while (( ich = reader.read() ) != -1)
+        {
+            ch = (char)ich; // 读入了一个字符，接下来要开始生产 Token 了
+            switch (state) // 状态机当前状态
+            {
+                case Init:
                 case GE:
-                case ASSIG:
+                case Assign:
                 case Plus:
                 case Minus:
                 case Star:
@@ -166,46 +144,73 @@ public class Lexer {
                 case SemiColon:
                 case LeftParen:
                 case RightParen:
-                    state = initToken(ch);
+                case singleQuotes:
+                case doubleQuotes:
+                    state = changeState(ch);// 根据读入的 ch , 修改状态
+                    break;
+                case Identifier: // 如果已经是标识符状态
+                    if (isAlpha(ch) || isDigit(ch)) { // 如果是字母或数字，则继续保持标识符状态
+                        tokenTmpStr.append(ch);
+                    } else {
+                        state = changeState(ch);
+                    }
+                    break;
+                case GT: // > 状态
+                    if (ch == '=') {
+                        oneToken.type = TokenType.GE;  //转换成GE
+                        state = fsm_state.GE;
+                        tokenTmpStr.append(ch);
+                    } else {
+                        state = changeState(ch);
+                    }
+                    break;
+                case IntLiteral:
+                    if (isDigit(ch)) {
+                        tokenTmpStr.append(ch);
+                    } else {
+                        state = changeState(ch);
+                    }
                     break;
                 case Id_int1:
                     if (ch == 'n') {
-                        state = Lexer.Lexer.fsm_state.Id_int2;
-                        tokenText.append(ch);
+                        state = fsm_state.Id_int2;
+                        tokenTmpStr.append(ch);
                     } else if (isDigit(ch) || isAlpha(ch)){
-                        state = Lexer.Lexer.fsm_state.Identifier;
-                        tokenText.append(ch);
+                        state = fsm_state.Identifier;
+                        tokenTmpStr.append(ch);
                     } else {
-                        state = initToken(ch);
+                        state = changeState(ch);
                     }
                     break;
                 case Id_int2:
                     if (ch == 't') {
-                        state = Lexer.Lexer.fsm_state.Id_int3;
-                        tokenText.append(ch);
+                        state = fsm_state.Id_int3;
+                        tokenTmpStr.append(ch);
                     } else if (isDigit(ch) || isAlpha(ch)){
-                        state = Lexer.Lexer.fsm_state.Identifier;
-                        tokenText.append(ch);
+                        state = fsm_state.Identifier;
+                        tokenTmpStr.append(ch);
                     } else {
-                        state = initToken(ch);
+                        state = changeState(ch);
                     }
                     break;
                 case Id_int3:
                     if (isBlank(ch)) {
-                        token.type = TokenType.Int;
-                        state = initToken(ch);
+                        oneToken.type = TokenType.Int;
+                        state = changeState(ch);
                     } else{
-                        state = Lexer.Lexer.fsm_state.Identifier;    //切换回Id状态
-                        tokenText.append(ch);
+                        state = fsm_state.Identifier;    //切换回Id状态
+                        tokenTmpStr.append(ch);
                     }
                     break;
             }
         }
-        // 把最后一个token送进去
-        if (tokenText.length() > 0) {
-            initToken(ch);
+
+        // 文件已经读到末尾，直接将 tokenTmpStr 作为 token，加入到序列
+        if (tokenTmpStr.length() > 0) {
+            changeState(ch);
         }
-        return new SimpleTokenReader(tokens);
+
+        return new SimpleTokenReader(tokenList);
     }
 
     /**
@@ -213,75 +218,93 @@ public class Lexer {
      * 这个初始状态其实并不做停留，它马上进入其他状态
      * 开始解析的时候，进入初始状态；某个Token解析完毕，也进入初始状态，在这里把Token记下来，然后建立一个新的Token
      */
-    private Lexer.Lexer.fsm_state initToken(char ch) {
-        if (tokenText.length() > 0) {
-            token.text = tokenText.toString();
-            tokens.add(token);
-            tokenText = new StringBuffer();
-            token = new SimpleToken();
+    private fsm_state changeState(char ch) {
+        // 已经鉴定了一个 token 了, 加入到序列中
+        if (tokenTmpStr.length() > 0) {
+            oneToken.text = tokenTmpStr.toString();
+            tokenList.add(oneToken);
+            tokenTmpStr = new StringBuffer();
+            oneToken = new SimpleToken();
         }
 
-        Lexer.Lexer.fsm_state newState;
+        fsm_state newState;
         if (isAlpha(ch)) {
             if (ch == 'i') {
-                newState = Lexer.Lexer.fsm_state.Id_int1;
+                newState = fsm_state.Id_int1;
             } else {
-                newState = Lexer.Lexer.fsm_state.Identifier; //进入Id状态
+                newState = fsm_state.Identifier; // 进入Id状态
             }
-            token.type = TokenType.Identifier;
-            tokenText.append(ch);
+            oneToken.type = TokenType.Identifier;
+            tokenTmpStr.append(ch);
         } else if (isDigit(ch)) {
-            newState = Lexer.Lexer.fsm_state.IntLiteral;
-            token.type = TokenType.IntLiteral;
-            tokenText.append(ch);
+            newState = fsm_state.IntLiteral;
+            oneToken.type = TokenType.IntLiteral;
+            tokenTmpStr.append(ch);
         } else if (ch == '>') {
-            newState = Lexer.Lexer.fsm_state.GT;
-            token.type = TokenType.GT;
-            tokenText.append(ch);
+            newState = fsm_state.GT;
+            oneToken.type = TokenType.GT;
+            tokenTmpStr.append(ch);
         } else if (ch == '+') {
-            newState = Lexer.Lexer.fsm_state.Plus;
-            token.type = TokenType.Plus;
-            tokenText.append(ch);
+            newState = fsm_state.Plus;
+            oneToken.type = TokenType.Plus;
+            tokenTmpStr.append(ch);
         } else if (ch == '-') {
-            newState = Lexer.Lexer.fsm_state.Minus;
-            token.type = TokenType.Minus;
-            tokenText.append(ch);
+            newState = fsm_state.Minus;
+            oneToken.type = TokenType.Minus;
+            tokenTmpStr.append(ch);
         } else if (ch == '*') {
-            newState = Lexer.Lexer.fsm_state.Star;
-            token.type = TokenType.Star;
-            tokenText.append(ch);
+            newState = fsm_state.Star;
+            oneToken.type = TokenType.Star;
+            tokenTmpStr.append(ch);
         } else if (ch == '/') {
-            newState = Lexer.Lexer.fsm_state.Slash;
-            token.type = TokenType.Slash;
-            tokenText.append(ch);
+            newState = fsm_state.Slash;
+            oneToken.type = TokenType.Slash;
+            tokenTmpStr.append(ch);
         } else if (ch == ';') {
-            newState = Lexer.Lexer.fsm_state.SemiColon;
-            token.type = TokenType.SemiColon;
-            tokenText.append(ch);
+            newState = fsm_state.SemiColon;
+            oneToken.type = TokenType.SemiColon;
+            tokenTmpStr.append(ch);
         } else if (ch == '(') {
-            newState = Lexer.Lexer.fsm_state.LeftParen;
-            token.type = TokenType.LeftParen;
-            tokenText.append(ch);
+            newState = fsm_state.LeftParen;
+            oneToken.type = TokenType.leftParen;
+            tokenTmpStr.append(ch);
         } else if (ch == ')') {
-            newState = Lexer.Lexer.fsm_state.RightParen;
-            token.type = TokenType.RightParen;
-            tokenText.append(ch);
+            newState = fsm_state.RightParen;
+            oneToken.type = TokenType.rightParen;
+            tokenTmpStr.append(ch);
         } else if (ch == '=') {
-            newState = Lexer.Lexer.fsm_state.ASSIG;
-            token.type = TokenType.Assignment;
-            tokenText.append(ch);
+            newState = fsm_state.Assign;
+            oneToken.type = TokenType.Assign;
+            tokenTmpStr.append(ch);
+        } else if(ch == '\''){
+            newState = fsm_state.singleQuotes;
+            oneToken.type = TokenType.singleQuotes;
+            tokenTmpStr.append(ch);
+        } else if(ch == '\"'){
+            newState = fsm_state.doubleQuotes;
+            oneToken.type = TokenType.doubleQuotes;
+            tokenTmpStr.append(ch);
         } else {
-            newState = Lexer.Lexer.fsm_state.Initial; // skip all unknown patterns
+            if(ch == '\n'){
+                System.out.printf("skipped : %d \\n\n",(int)ch);
+            } else if(ch == ' '){
+                System.out.printf("skipped : %d \\b\n",(int)ch);
+            } else if(ch == '\t'){
+                System.out.printf("skipped : %d \\t\n",(int)ch);
+            } else{
+                System.out.printf("skipped unknown character: %d %c\n",(int)ch, ch);
+            }
+            newState = fsm_state.Init;
         }
         return newState;
     }
 
     // 打印所有的Token
     public static void dump(SimpleTokenReader tokenReader){
-        System.out.println("text\ttype");
+        System.out.println("text\t\t\type\n------------------------");
         Token token = null;
-        while ((token= tokenReader.read())!=null){
-            System.out.println(token.getText()+"\t"+token.getType());
+        while ((token = tokenReader.read())!=null){
+            System.out.println(token.getText()+"\t\t\t"+token.getType());
         }
     }
 
--- Lexer/TokenType.java
+++ Lexer/TokenType.java
@@ -1,23 +1,27 @@
 package Lexer;
 
 public enum TokenType{
-    Plus,   // +
-    Minus,  // -
-    Star,   // *
-    Slash,  // /
-    GE,     // >=
-    GT,     // >
-    EQ,     // ==
-    LE,     // <=
-    LT,     // <
-    SemiColon, // ;
-    LeftParen, // (
-    RightParen,// )
-    Assignment,// =
-    If,
-    Else,
-    Int,
-    Identifier,     //标识符
-    IntLiteral,     //整型字面量
-    StringLiteral   //字符串字面量
+    Plus,            // +
+    Minus,           // -
+    Star,            // *
+    Slash,           // /
+    GE,              // >=
+    GT,              // >
+    EQ,              // ==
+    LE,              // <=
+    LT,              // <
+    SemiColon,       // ;
+    singleQuotes,    // '
+    doubleQuotes,    // "
+    leftCurlyBrace,  // {
+    rightCurlyBrace, // {
+    leftParen,       // (
+    rightParen,      // )
+    Assign,          // =
+    If,              // if
+    Else,            // else
+    Int,             // int
+    Identifier,      // 标识符
+    IntLiteral,      // 整型字面量
+    StringLiteral    // 字符串字面量
 }
